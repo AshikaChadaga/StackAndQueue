8da2c29e5108a70a158ca03cd09f35a348bcdf26
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-31 20:24:52 +0530

    ADDED: Welcome Message

---
+++ src/com/bridgelabz/stackandqueue/StackMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.stackandqueue;
+
+import com.bridgelabz.linkedlist.*;
+
+public class StackMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("----- Welcome To Data Structure : Stack -----");
+
+	}
+
+}
