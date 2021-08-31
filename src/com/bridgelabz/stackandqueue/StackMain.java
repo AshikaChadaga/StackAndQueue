4eeb731d64af01c2b451f76a3afbab6cc06217cf
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-31 20:30:11 +0530

    MODIFIED: Demonstration of Stack operations

--- src/com/bridgelabz/stackandqueue/StackMain.java
+++ src/com/bridgelabz/stackandqueue/StackMain.java
@@ -8,6 +8,15 @@ public class StackMain {
 		
 		System.out.println("----- Welcome To Data Structure : Stack -----");
 
+		Stack stackList = new Stack();
+		MyNode<Integer> firstNode = new MyNode<Integer>(70);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+		stackList.push(firstNode);
+		stackList.push(secondNode);
+		stackList.push(thirdNode);
+		stackList.printStack();
+
 	}
 
 }
