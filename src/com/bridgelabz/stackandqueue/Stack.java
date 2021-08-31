2eb79de123c6cf022611277855b35ea60ad9ce8d
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-31 20:28:54 +0530

    ADDED: Stack class

---
+++ src/com/bridgelabz/stackandqueue/Stack.java
@@ -0,0 +1,47 @@
+package com.bridgelabz.stackandqueue;
+
+import com.bridgelabz.linkedlist.*;
+
+public class Stack<K> {
+	
+	private final LinkedList stackLinkedList;
+	
+	public Stack() {
+		stackLinkedList = new LinkedList();
+	}
+	
+	
+	public void push(INode element) {
+		stackLinkedList.add(element);
+	}
+	
+	
+	public INode peek() {
+		return stackLinkedList.head;
+	}
+	
+	
+	public INode pop() {
+		return stackLinkedList.deleteFirst();
+	}
+	
+	
+	public boolean isEmpty() {
+		if(stackLinkedList.head != null) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+	
+	public int size() {
+		return stackLinkedList.size();
+	}
+	
+	
+	public void printStack() {
+		stackLinkedList.printLinkedList();
+	}
+}
