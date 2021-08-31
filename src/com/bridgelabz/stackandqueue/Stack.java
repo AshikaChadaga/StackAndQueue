d71e5acbddf5bfcafb61e4c027ede62210236d11
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-31 20:31:57 +0530

    ADDED: Stack class having push operation

--- src/com/bridgelabz/stackandqueue/Stack.java
+++ src/com/bridgelabz/stackandqueue/Stack.java
@@ -14,32 +14,7 @@ public class Stack<K> {
 	public void push(INode element) {
 		stackLinkedList.add(element);
 	}
-	
-	
-	public INode peek() {
-		return stackLinkedList.head;
-	}
-	
-	
-	public INode pop() {
-		return stackLinkedList.deleteFirst();
-	}
-	
-	
-	public boolean isEmpty() {
-		if(stackLinkedList.head != null) {
-			return false;
-		}
-		else {
-			return true;
-		}
-	}
-	
-	
-	public int size() {
-		return stackLinkedList.size();
-	}
-	
+
 	
 	public void printStack() {
 		stackLinkedList.printLinkedList();
